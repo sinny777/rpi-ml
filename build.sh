3a6fe81fb08fe60d5bb5f64228efcc0aa4805525
sinny777 <sinny777@gmail.com>
2018-05-30 00:14:33 +0530

    few changes...

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ function install_build_tools {
   apt-get update
   apt-get dist-upgrade
   apt-get install build-essential tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev
+  apt-get install python3
+  apt-get install python3-pip
 }
 
 function install_node {
@@ -11,16 +13,6 @@ function install_node {
   tar -xzf node-v8.9.0-linux-armv6l.tar.gz
   cd node-v8.9.0-linux-armv6l/
   sudo cp -R * /usr/local/
-}
-
-function install_python3 {
-    cd /root
-    wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tar.xz
-    tar xf Python-3.6.5.tar.xz
-    cd Python-3.6.5
-    ./configure
-    make
-    sudo make altinstall
 }
 
 function install_ml_dependencies {
@@ -33,7 +25,7 @@ function install_ml_dependencies {
 install_build_tools
 install_node
 install_python3
-install_ml_dependencies
+# install_ml_dependencies
 # install_app_dependencies
 # configure_ldconfig
 # build_psips
